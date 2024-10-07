6f3607d624565b4ff043a15f872ea5a4c17b0fc1
bustlingbungus <cadens@usf.edu>
2024-10-07 09:26:02 -0400

    updated comment on main

--- main.py
+++ main.py
@@ -1,30 +1,6 @@
 import pygame, sys
 from Classes import Vector2, Ball, GRAVITY_STRENGTH, BALL_COLOR, BALL_RADIUS, gravity, BACKGROUND_COLOR, BRUSH_SIZE
 
-
-'''
-ISSUES:
-
-In the C version, the program is set to run at a fixed framerate (120fps). I don't know how to do that in pygame,
-so I just added 'CLOCK.tick(300)' at the end, which is just wasiting for 0.3s, not atually fixing the framerate.
-This discrepancy leads to some very unnatural physics
-
-Input is broken, you should be able to press arrow keys to change gravity, and return to disable it. I don't know 
-why this doesn't work, the code for it is in 'get_input'
-
-Right clicking allows you to delete entities. For some reason, right clicking once will put your mouse in "delete 
-mode" forever, even when you release right click. I don't know why this is, the code for it is also in 'get_input'
-
-Lag: the C version can handle upwards of 500 entities before encountering serious lag. This version can only handle
-around 140. Part of this is likely just Python being inherently slower, but it is also likely due to my own
-inexperience with Python programming. If someone could figure out a way to optimise this code, that would be good
-
-(I'm talking minor optimisations, no fixing the lag by optimising collision detection by dividing the area into 
-chunks or anything like that, it would get too complicated for the workshop T-T)
-'''
-
-
-
 pygame.init()
 
 left_mouseDown = False
